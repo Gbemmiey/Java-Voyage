3ab07569e9a3f51fac979cf1602d2df4ddc3e62a
Oluwagbemileke Femi Oyeyoade <gbemfemalabi@gmail.com>
2023-06-29 19:01:27 +0100

    Update: Implemented Dynamic Polymorphism

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
-
-
         Student s1 = new Student("Karan", 1);
-        System.out.println(s1.getDetails());
-//        System.out.println(s1.computeFees(10));
-//        System.out.println(s1.computeFees());
-
         Student s2 = new ResearchStudent("Ade", 2, "Mathematics");
-        System.out.println(s2.getDetails());
-
         Student s3 = new phdStudent("Kunle", 4, "Mathematics", "Mr. Onobote", "Discrete Mathematics");
-        System.out.println(s3.getDetails());
+        Student[] students = new Student[]{s1, s2, s3};
+        PrintDetails(students);
+    }
+
+    public static void PrintDetails(Student[] students) {
+        for (Student student : students) {
+            System.out.println(student.getDetails());
+        }
     }
 }
 
