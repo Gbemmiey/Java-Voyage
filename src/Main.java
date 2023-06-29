9373bf4934425b4708536bde56f75b91fa734897
Oluwagbemileke Femi Oyeyoade <gbemfemalabi@gmail.com>
2023-06-29 17:41:00 +0100

    Update: Implemented Composition

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,33 @@
 public class Main {
 
     public static void main(String[] args) {
-        Student s1 = new Student("Karan", 2);
+
+        BankAccount b1 = new BankAccount(2000.50f, 220202, "Optimus Bank");
+        BankAccount b2 = new BankAccount(1050.00f, 110101, "Access Bank");
+        BankAccount b3 = new BankAccount(20000.00f, 440404, "Jaiz Bank");
+
+        Student s1 = new Student("Karan", 1, b1);
         System.out.println(s1.getDetails());
 
-        ResearchStudent s2 = new ResearchStudent("Ade", 2, "Mathematics");
+        ResearchStudent s2 = new ResearchStudent("Ade", 2, b2, "Mathematics");
         System.out.println(s2.getDetails());
+
+        phdStudent s3 = new phdStudent("Kunle", 4, "Mathematics", b3, "Mr. Onobote", "Discrete Mathematics");
+        System.out.println(s3.getDetails());
     }
 }
 
 class Student {
-    private final int annualFees = 10000;
+    private static final int annualFees = 10000;
     private final String name;
     private final int year;
 
-    public Student(String name, int year) {
+    private final BankAccount bankAccount;
+
+    public Student(String name, int year, BankAccount bankAccount) {
         this.name = name;
         this.year = year;
+        this.bankAccount = bankAccount;
     }
 
     public String getDetails() {
@@ -24,19 +35,44 @@ class Student {
     }
 
     public float computeFees() {
-        return this.annualFees * this.year;
+        return annualFees * this.year;
     }
 
 }
 
 class ResearchStudent extends Student {
-    private static final int annualFees = 20000;
     private final String researchArea;
 
-
-    public ResearchStudent(String name, int year, String researchArea) {
-        super(name, year);
+    public ResearchStudent(String name, int year, BankAccount bankAccount,
+                           String researchArea) {
+        super(name, year, bankAccount);
         this.researchArea = researchArea;
+    }
+}
+
+class phdStudent extends ResearchStudent {
+    private final String supervisor;
+    private final String thesisTitle;
+
+    public phdStudent(String name, int year, String researchArea,
+                      BankAccount bankAccount,
+                      String supervisor, String thesisTitle) {
+        super(name, year, bankAccount, researchArea);
+        this.supervisor = supervisor;
+        this.thesisTitle = thesisTitle;
+
+    }
+}
+
+class BankAccount {
+    public float accountBalance;
+    public long accountNumber;
+    public String bankName;
+
+    public BankAccount(float accountBalance, long accountNumber, String bankName) {
+        this.accountBalance = accountBalance;
+        this.accountNumber = accountNumber;
+        this.bankName = bankName;
     }
 
 }
