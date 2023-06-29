b22f3f32525ae08e87cff0ef1bea019e2a34e598
Oluwagbemileke Femi Oyeyoade <gbemfemalabi@gmail.com>
2023-06-29 16:47:36 +0100

    Update: Multiple classes in same file

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,54 @@
-import java.util.Scanner;
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        int A, B, C;
-        System.out.print("Hello and welcome!");
+        Student s1 = new Student("Karan", 2);
+        System.out.println(s1.getDetails());
 
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Enter A: ");
-        A = input.nextInt();
-
-        System.out.println("Enter B: ");
-        B = input.nextInt();
-
-        System.out.println("Enter C: ");
-        C = input.nextInt();
-
-        String output = String.format("%d + %d + %d = %d", A, B, C, A + B + C);
-        System.out.println(output);
+        ResearchStudent s2 = new ResearchStudent("Ade", 2, "Mathematics");
+        System.out.println(s2.getDetails());
     }
+}
+
+class Student {
+    private final int annualFees = 10000;
+    private final String name;
+    private final int year;
+
+    public Student(String name, int year) {
+        this.name = name;
+        this.year = year;
+    }
+
+    public String getDetails() {
+        return "Name : " + this.name + '\n' + "Fees : " + computeFees();
+    }
+
+    public float computeFees() {
+        return this.annualFees * this.year;
+    }
+
+}
+
+class ResearchStudent {
+    private static final int annualFees = 10000;
+    private final String name;
+    private final int year;
+    private final String researchArea;
+
+    public ResearchStudent(String name, int year, String researchArea) {
+        this.name = name;
+        this.year = year;
+        this.researchArea = researchArea;
+    }
+
+    public String getDetails() {
+        return "Name : " + this.name + '\n'
+                + "Fees : " + computeFees()
+                + '\n' + "Area : " + this.researchArea;
+    }
+
+    public float computeFees() {
+        return annualFees * this.year;
+    }
+
 }
