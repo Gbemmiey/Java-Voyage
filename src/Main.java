e891190ef619db1de1f5835012b65c6fb4a0ef65
Oluwagbemileke Femi Oyeyoade <gbemfemalabi@gmail.com>
2023-06-30 13:59:05 +0100

    Update: Implemented Type-safe Arraylist

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        Student s1 = new Student("Karan", 1);
-        Student s2 = new ResearchStudent("Ade", 2, "Mathematics");
-        Student s3 = new phdStudent("Kunle", 4, "Mathematics", "Mr. Onobote", "Discrete Mathematics");
-        Student[] students = new Student[]{s1, s2, s3};
-        PrintDetails(students);
+
+        ArrayList<Student> studentList = new ArrayList<Student>();
+
+        studentList.add(new Student("Karan", 1));
+        studentList.add(new ResearchStudent("Ade", 2, "Mathematics"));
+        studentList.add(new phdStudent("Kunle", 4, "Mathematics", "Mr. Onobote", "Discrete Mathematics"));
+        PrintDetails(studentList);
     }
 
-    public static void PrintDetails(Student[] students) {
-        for (Student student : students) {
-            System.out.println(student.getDetails());
-            System.out.println(student.computeFees());
+    public static void PrintDetails(ArrayList<Student> students) {
+        for (Student s : students) {
+            System.out.println(s.getDetails());
+            System.out.println(s.computeFees());
         }
     }
 }
