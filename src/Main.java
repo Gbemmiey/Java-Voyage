67e2a7269887c637041a12cb01c807d00c602ee0
Oluwagbemileke Femi Oyeyoade <gbemfemalabi@gmail.com>
2023-06-30 14:07:48 +0100

    Update: Implemented Type-safe Arraylist remove method

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,19 @@ public class Main {
         ArrayList<Student> studentList = new ArrayList<Student>();
 
         studentList.add(new Student("Karan", 1));
-        studentList.add(new ResearchStudent("Ade", 2, "Mathematics"));
+
+        // Add to ArrayList with Index
+        studentList.add(0, new ResearchStudent("Ade", 2, "Mathematics"));
+
         studentList.add(new phdStudent("Kunle", 4, "Mathematics", "Mr. Onobote", "Discrete Mathematics"));
         PrintDetails(studentList);
+
+        System.out.println("\n \n \n");
+
+        studentList.remove(2);
+        PrintDetails(studentList);
+        
+
     }
 
     public static void PrintDetails(ArrayList<Student> students) {
